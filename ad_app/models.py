d8007c6d3ab381e81a447304c7c2216b4cee4b30
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-28 12:33:57 +0700

    create Ad model

---
+++ ad_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Ad(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.TextField(max_length=100)
+    amount_of_views = models.IntegerField()
+    position = models.IntegerField()
+
+    def __str__(self):
+        return self.title
